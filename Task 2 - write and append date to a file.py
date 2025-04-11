eab56abe41d41d872863a71dfda4f1f7bfce8155
senmihir12 <senmihir12@gmail.com>
2025-04-11 15:43:56 +0530

    Update Task 2 - write and append date to a file.py

--- Task 2 - write and append date to a file.py
+++ Task 2 - write and append date to a file.py
@@ -1,13 +1,14 @@
 start_input = input("Enter text to write to the file: ")
 with open("output.txt", "w") as file:
     file.write(start_input + "\n")
+    print("Data successfully written to output.txt\n")
 
 append_input = input("Enter additional text to append: ")
 with open("output.txt", "a") as file:
     file.write(append_input + "\n")
+    print("Data successfully appended\n")
 
-print("\nFinal content of output.txt:")
+print("Final content of output.txt:")
 with open("output.txt", "r") as file:
     for line in file:
         print(line.strip())
-
